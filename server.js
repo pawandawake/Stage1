b8fb964d9f60ff309bf9d725ff06b546180a364d
Pawan <psdawake@gmail.com>
2023-09-10 21:29:38 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/api/', (req, res) => {
+  // Handle the request and generate a response.
+  const slack_name = req.query.slack_name;
+  const track = req.query.track;
+
+  const currentDate = new Date();
+  const currentDay = currentDate.getDay();
+  const time= currentDate.toISOString()
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDayName = daysOfWeek[currentDay];
+  
+res.json({
+  "slack_name": slack_name,
+  "current_day": currentDayName,
+  "utc_time": time,
+  "track": track,
+  "github_file_url": "https://github.com/Benrobo/hngx-be/blob/main/s-1/app.go",
+  "github_repo_url": "https://github.com/benrobo/hngx-be",
+  "status_code": 200 
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
